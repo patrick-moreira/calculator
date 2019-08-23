4d9b649b50cbe58049cd6e751fc5c505022afada
patrick-moreira <patrickmoreira1995@gmail.com>
2019-08-23 14:27:50 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Calculator = () => {
+  return (
+    <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Calculator</Text>
+    </View>
+  );
+}
+
+export default Calculator; 
+
